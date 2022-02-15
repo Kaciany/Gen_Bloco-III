8c2da2c91bfec0d916816724d7b15ed5cef2f8e3
Kaciany Ramos <Kaciany2012@hotmail.com>
2022-02-15 15:14:59 -0300

    Atividade react.

---
+++ Projeto/blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+return(
+    <>
+    <h1 className='titulo'>Home</h1>
+   <img src="https://i.imgur.com/L7jFatC.jpg" alt="Imagem tela inicial" className='img' />
+    
+    </>
+);
+}
+
+export default Home;
